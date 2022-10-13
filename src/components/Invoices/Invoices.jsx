1f80d42214e485a9b1601751a07a1b1403e4dac9
LuckyYada15 <20bcs077@iiitdwd.ac.in>
2022-10-14 03:47:34 +0530

    data added and tags added

--- src/components/Invoices/Invoices.jsx
+++ src/components/Invoices/Invoices.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { AiOutlineArrowUp } from "react-icons/ai"
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -13,12 +15,13 @@ import { Input, Radio, Pagination, Table, Divider } from "antd";
 import { Card, Button, Col, Row, Statistic } from "antd";
 import { Layout, Menu, Typography } from "antd";
 import Icon, { DownOutlined, UpOutlined } from "@ant-design/icons";
-import { Dropdown, Space } from "antd";
+import { Dropdown, Space, Tag } from "antd";
 
 import "antd/dist/antd.css";
 import "../DashboardLayout/dashboard-layout.css";
 import background from "../../resources/search-icon-SBI-300266455-preview.jpg";
 
+const { Column, ColumnGroup } = Table;
 const { Header, Sider, Content } = Layout;
 const { Search } = Input;
 const { Title } = Typography;
@@ -42,10 +45,11 @@ const maincolumn = [
 const maindata = [
   {
     sales_amount: "Rs 20,000",
-    total_sales: "Rs 20,000",
-    paid: "Rs 10,000"
+    total_sales: "Rs 110,000",
+    paid: "Rs 70,000"
   }
 ]
+
 const columns = [
   {
     title: "DATE",
@@ -81,211 +85,99 @@ const columns = [
   },
   {
     title: "",
-    dataIndex: "button",
-  },
-  // {
-  //   title: "BALANCE DUE",
-  //   dataIndex: "balance_due",
-  //   sorter: {
-  //     compare: (a, b) => a.inv_no - b.inv_no,
-  //     multiple: 3,
-  //   },
-  // },
-  // {
-  //   title: "STATUS",
-  //   dataIndex: "status",
-  //   filters: [
-  //     {
-  //       text: 'Partially Paid',
-  //       value: 'Partially Paid',
-  //     },
-  //     {
-  //       text: 'Not Overdue',
-  //       value: 'Not Overdue',
-  //     },
-  //     {
-  //       text: 'Paid',
-  //       value: 'Paid',
-  //     },
-  //     {
-  //       text: 'Overdue',
-  //       value: 'Overdue',
-  //     },
-  //     {
-  //       text: 'Due Today',
-  //       value: 'Due Today',
-  //     },
-  //     {
-  //       text: 'Created Today',
-  //       value: 'Created Today',
-  //     },
-  //   ],
-  //   onFilter: (value, record) => record.name.indexOf(value) === 0,
-  // },
-  // {
-  //   title: "ACKNOWLEDGED",
-  //   dataIndex: "acknowledged",
-  // },
-  // {
-  //   title: "SEND INVOICE",
-  //   dataIndex: "send_invoice",
-  // },
+    dataIndex: "tags",
+    key: "tags",
+  }
 ];
 
 const data = [
   {
     key: "1",
     date: "John Brown",
-    inv_no: 1,
-    customer_name: 60,
+    sales_invoice: 1,
+    party_name: "Arjun Singh",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "2",
     date: "Jim Green",
-    inv_no: 2,
-    customer_name: 66,
+    sales_invoice: 2,
+    party_name: "Bhanu Prakash",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Paid"]
   },
   {
     key: "3",
     date: "Joe Black",
-    inv_no: 3,
-    customer_name: 90,
+    sales_invoice: 3,
+    party_name: "Chintan Chabra",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "4",
     date: "Jim Red",
-    inv_no: 4,
-    customer_name: 99,
+    sales_invoice: 4,
+    party_name: "Dean Dio",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "5",
     date: "Jim Red",
-    inv_no: 5,
-    customer_name: 99,
+    sales_invoice: 5,
+    party_name: "Jasmine Kapoor",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "6",
     date: "Jim Red",
-    inv_no: 6,
-    customer_name: 99,
+    sales_invoice: 6,
+    party_name: "Zaid Khan",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Paid"],
   },
   {
     key: "7",
     date: "Jim Red",
-    inv_no: 7,
-    customer_name: 99,
+    sales_invoice: 7,
+    party_name: "Kotla Khan",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "8",
     date: "Jim Red",
-    inv_no: 8,
-    customer_name: 99,
+    sales_invoice: 8,
+    party_name: "Rishi Yadav",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "9",
     date: "Jim Red",
-    inv_no: 9,
-    customer_name: 99,
+    sales_invoice: 9,
+    party_name: "Rohan Kapoor",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "10",
     date: "Jim Red",
-    inv_no: 10,
-    customer_name: 99,
+    sales_invoice: 10,
+    party_name: "Sanjiv Rathee",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
   {
     key: "11",
     date: "Jim Red",
-    inv_no: 11,
-    customer_name: 99,
+    sales_invoice: 11,
+    party_name: "Tarun Sharma",
     amount: 70,
-    due_date: 70,
-    balance_due: 70,
-    invoice_type: 70,
-    status: 70,
-    acknowledged: 70,
-    send_invoice: 70,
-    button: "Unpaid"
+    tags: ["Unpaid"]
   },
 ];
 
@@ -581,14 +473,25 @@ const Invoices = () => {
                   >
                     <div>
                       <h2 style={{ fontSize: '25px', fontWeight: '900', marginTop: "10px" }}>Sales Invoices</h2>
-                      <h1 style={{ fontSize: '15px' , marginTop: "-8%"}}>Bill Your Sales to Customers</h1>
+                      <h1 style={{ fontSize: '15px', marginTop: "-8%" }}>Bill Your Sales to Customers</h1>
                     </div>
                     <div style={{
                       display: "flex", width: "60px",
                       marginLeft: "auto",
-                      marginRight: 0,
+                      marginRight: "70px",
                       marginTop: "32px"
                     }}>
+                      <Button
+                        type="primary"
+                        style={{
+                          margin: "0 1vw",
+                          backgroundColor: "#d8d7df",
+                          color: "#000000",
+                          border: "none",
+                        }}
+                      ><a style={{ color: "#000000" }}>Invoice Settings</a>&nbsp;
+                        <FiSettings />
+                      </Button>
                       <Button
                         type="primary"
                         style={{
@@ -597,30 +500,15 @@ const Invoices = () => {
                           border: "none",
                         }}
                       >
-                        Send Bulk Messages
+                        <AiOutlineArrowUp />&nbsp;<a style={{ color: "#ffffff" }}>Create Sales Invoices</a>
                       </Button>
-                      <Dropdown
-                        overlay={menu2}
-                        trigger={["click"]}
-                        style={{ border: "2px black" }}
-                        onClick={() => setDroppeddown2(!droppeddown2)}
-                      >
-                        <a onClick={(e) => e.preventDefault()}>
-                          <Space>
-                            <Button style={{ color: "rgb(22, 28, 75)" }}>
-                              Export
-                              <DownOutlined />
-                            </Button>
-                          </Space>
-                        </a>
-                      </Dropdown>
                     </div>
 
                   </div>
                 </div>
               </Row>
               <hr />
-              <Row style={{ marginBottom: "1%" , marginTop: "-3%"}}>
+              <Row style={{ marginBottom: "1%", marginTop: "-3%" }}>
                 <div style={{ display: "flex" }}>
                   <Input
                     placeholder="Search by Debtor Name"
@@ -656,10 +544,9 @@ const Invoices = () => {
                   />
                 </div>
               </Row>
-
-
               <Table
                 style={{ marginLeft: "0%", marginRight: "70%", paddingBottom: "1%" }}
+                tags={maindata}
                 columns={maincolumn}
                 dataSource={maindata}
                 onChange={onChangeTableValues}
@@ -667,15 +554,25 @@ const Invoices = () => {
                   false
                 }
               />
-              <Table
-                style={{ marginRight: "2%" }}
-                columns={columns}
-                dataSource={data}
-                onChange={onChangeTableValues}
-                pagination={{
-                  size: "small",
-                }}
-              />
+              <Table dataSource={data}>
+                <Column title="Date" dataIndex="date" key="date" />
+                <Column title="Sales Invoice No" dataIndex="sales_invoice" key="sales_invoice" />
+                <Column title="Party Name" dataIndex="party_name" key="party_name" />
+                <Column title="Amount" dataIndex="amount" key="amount" />
+                <Column
+                  title=""
+                  dataIndex="tags"
+                  key="tags"
+                  
+                  render={(tag) => (
+                    <>
+                    <Tag  color="blue" key={tag}>
+                          {tag}
+                        </Tag>
+                    </>
+                  )}
+                />
+              </Table>
             </div>
           </Layout>
         </Layout>
